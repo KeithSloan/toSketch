333c99ccce7ec97088ca96735bb66fea89717c2e
Keith Sloan <keith@sloan-home.co.uk>
2021-01-03 00:21:44 +0000

    Initial

---
+++ freecad/Face2Sketch/F2SCommands.py
@@ -0,0 +1,82 @@
+#**************************************************************************
+#*                                                                        *
+#*   Copyright (c) 2021 Keith Sloan <keith@sloan-home.co.uk>              *
+#*                                                                        *
+#*   This program is free software; you can redistribute it and/or modify *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)   *
+#*   as published by the Free Software Foundation; either version 2 of    *
+#*   the License, or (at your option) any later version.                  *
+#*   for detail see the LICENCE text file.                                *
+#*                                                                        *
+#*   This program is distributed in the hope that it will be useful,      *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+#*   GNU Library General Public License for more details.                 *
+#*                                                                        *
+#*   You should have received a copy of the GNU Library General Public    *
+#*   License along with this program; if not, write to the Free Software  *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 *
+#*   USA                                                                  *
+#*                                                                        *
+#*   Acknowledgements :                                                   *
+#*                                                                        *
+#**************************************************************************
+
+__title__="FreeCAD Face2Sketch Workbench - GUI Commands"
+__author__ = "Keith Sloan"
+__url__ = ["http://www.freecadweb.org"]
+
+'''
+This Script includes the GUI Commands of the GDML module
+'''
+
+import FreeCAD,FreeCADGui
+from PySide import QtGui, QtCore
+
+class Face2SketchFeature:
+    #    def IsActive(self):
+    #    return FreeCADGui.Selection.countObjectsOfType('Part::Feature') > 0
+
+    def Activated(self):
+        #from .GDMLObjects import GDMLBox, ViewProvider
+#        for obj in FreeCADGui.Selection.getSelection():
+        for sel in FreeCADGui.Selection.getSelectionEx() :
+            #if len(obj.InList) == 0: # allowed only for for top level objects
+            #cycle(obj)
+            print("Selected")
+            print(sel.HasSubObjects)
+            print(sel.Object)
+            print(sel.Object.Label)
+            print(sel.SubObjects)
+            print(sel.SubObjects[0])
+            print(sel.SubObjects[0].ShapeType)
+            print(sel.SubObjects[0].Surface)
+            if str(sel.SubObjects[0].Surface) == '<Plane object>' :
+               print('Planar')
+               #print(dir(sel.SubObjects[0].Wires))
+               #print(dir(sel.SubObjects[0].SubShapes))
+               print(dir(sel.SubObjects[0].Surface))
+               shape = sel.SubObjects[0].Surface.toShape()
+               print(shape)
+               print('Print Dir Shape')
+               print(dir(shape))
+               shape.exportStep('/tmp/exported.step')
+               shape.exportBrep('/tmp/exported.brep')
+
+            print(dir(sel.SubObjects))
+            print(dir(sel.SubObjects[0]))
+
+    def IsActive(self):
+        if FreeCAD.ActiveDocument == None:
+           return False
+        else:
+           return True
+
+    def GetResources(self):
+        return {'Pixmap'  : 'Face2SketchFeature', 'MenuText': \
+                QtCore.QT_TRANSLATE_NOOP('Face2SketchFeature',\
+                'Face 2 Sketch'), 'ToolTip': \
+                QtCore.QT_TRANSLATE_NOOP('Face2SketchFeature',\
+                'Face 2 Sketch')}
+
+FreeCADGui.addCommand('Face2SketchCommand',Face2SketchFeature())
---
+++ freecad/Face2Sketch/init_gui.py
@@ -0,0 +1,85 @@
+# Face2Sketch wrkbench gui init module
+#
+# Gathering all the information to start FreeCAD
+# This is the second one of three init scripts, the third one
+# runs when the gui is up
+
+#***************************************************************************
+#*   (c) Juergen Riegel (juergen.riegel@web.de) 2002                       *
+#*                                                                         *
+#*   This file is part of the FreeCAD CAx development system.              *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   FreeCAD is distributed in the hope that it will be useful,            *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Lesser General Public License for more details.                   *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with FreeCAD; if not, write to the Free Software        *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#*   Juergen Riegel 2002                                                   *
+#*                                                                         *
+#* Also copyright Keith Sloan                                              * 
+#***************************************************************************/
+
+#import FreeCAD
+#from FreeCAD import *
+import FreeCAD
+#import PartGui
+import FreeCADGui
+#from freecad.gdml import F2SCommands, F2SResources
+from freecad.Face2Sketch import F2SCommands
+
+def joinDir(path) :
+    import os
+    __dirname__ = os.path.dirname(__file__)
+    return(os.path.join(__dirname__,path))
+
+class Face2Sketch_Workbench ( FreeCADGui.Workbench ):
+
+#    import FreeCAD
+
+    "Face2Sketch workbench object"
+    def __init__(self):
+        self.__class__.Icon = joinDir("Resources/icons/face2SketchWorkbench.svg")
+        self.__class__.MenuText = "Face2Sketch"
+        self.__class__.ToolTip = "Face2Sketch workbench"
+
+    def Initialize(self):
+        def QT_TRANSLATE_NOOP(scope, text):
+            return text
+        
+        #import GDMLCommands, GDMLResources
+        #import F2SCommands
+        commands=['Face2SketchCommand']
+
+        toolbarcommands=['Face2SketchCommand']
+
+        self.appendToolbar(QT_TRANSLATE_NOOP('Workbench','F2S_Tools'),toolbarcommands)
+        self.appendMenu('Face2Sketch',commands)
+        FreeCADGui.addIconPath(joinDir("Resources/icons"))
+        FreeCADGui.addLanguagePath(joinDir("Resources/translations"))
+        #FreeCADGui.addPreferencePage(joinDir("Resources/ui/Face2Sketch-base.ui"),"Face2Sketch")
+
+    def Activated(self):
+        "This function is executed when the workbench is activated"
+        print ("Activated")
+        return
+
+    def Deactivated(self):
+        "This function is executed when the workbench is deactivated"
+        return
+    
+    def GetClassName(self):
+        return "Gui::PythonWorkbench"
+
+FreeCADGui.addWorkbench(Face2Sketch_Workbench())
+
