df4929238d5734abe354e7b910b44873fd2c2e17
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-11-24 12:36:02 -0500

    Fixup copyright headers
    .

--- freecad/toSketch/toSCommands.py
+++ freecad/toSketch/toSCommands.py
@@ -1,29 +1,26 @@
-# SPDX-License-Identifier: LGPL-2.0-or-later
+# SPDX-License-Identifier: LGPL-2.1-or-later
 # SPDX-FileNotice: Part of the ToSketch addon.
 
-#**************************************************************************
-#*                                                                        *
-#*   Copyright (c) 2021 Keith Sloan <keith@sloan-home.co.uk>              *
-#*                                                                        *
-#*   This program is free software; you can redistribute it and/or modify *
-#*   it under the terms of the GNU Lesser General Public License (LGPL)   *
-#*   as published by the Free Software Foundation; either version 2 of    *
-#*   the License, or (at your option) any later version.                  *
-#*   for detail see the LICENCE text file.                                *
-#*                                                                        *
-#*   This program is distributed in the hope that it will be useful,      *
-#*   but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-#*   GNU Library General Public License for more details.                 *
-#*                                                                        *
-#*   You should have received a copy of the GNU Library General Public    *
-#*   License along with this program; if not, write to the Free Software  *
-#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 *
-#*   USA                                                                  *
-#*                                                                        *
-#*   Acknowledgements :                                                   *
-#*                                                                        *
-#**************************************************************************
+################################################################################
+#                                                                              #
+#   Copyright (c) 2021 Keith Sloan ( keith@sloan-home.co.uk )                  #
+#                                                                              #
+#   This library is free software; you can redistribute it and/or modify it    #
+#   under the terms of the GNU Lesser General Public License as published      #
+#   by the Free Software Foundation; either version 2.1 of the License, or     #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This library is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       #
+#                                                                              #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public License   #
+#   along with this library; if not, write to the Free Software Foundation,    # 
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA           #
+#                                                                              #
+################################################################################
 
 __title__="FreeCAD Face2Sketch Workbench - GUI Commands"
 __author__ = "Keith Sloan"
